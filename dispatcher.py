76498a4fdbfd336af2b495c5bec5828bf9d17bd6
Alejandromarchesini12 <alejandro.marchesini12@gmail.com>
2025-11-18 10:50:33 -0800

    Add ready queue and logic for it in tick

--- dispatcher.py
+++ dispatcher.py
@@ -1,16 +1,23 @@
 from thread import Thread
+from collections import deque
 
 class Dispatcher:
 	def __init__(self, threads: list[Thread], quantum: int = 2):
 		self.time_step: int = 0
 		self.threads: list[Thread] = threads
 		self.quantum: int = quantum  # Default quantum time
+		self.ready_queue: deque[Thread] = deque() # Holds Thread objects that have arrived and are waiting to run
+		self.gantt_chart: list[tuple[str,int]] = [] # store tuples representing who ran at each time unit
 
 	def add_thread(self, thread: Thread):
 		self.threads.append(thread)
 
 	def tick(self):
 		self.time_step += 1
+		# Add newly arrived threads to the ready queue
+		for th in self.threads: 
+			if th.arrival == self.time_step:
+				self.ready_queue.append(th)
 
 	def reset(self) -> None:
 		self.time_step = 0
