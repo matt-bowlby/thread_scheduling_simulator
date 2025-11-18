3bcba6242af6cb22a1ff7585537b88044d5df629
matt-bowlby <mbowlby794@gmail.com>
2025-11-17 22:12:32 -0800

    Add static types to variables in Thread class

--- thread/thread.py
+++ thread/thread.py
@@ -1,33 +1,33 @@
-class Thread: 
+class Thread:
     '''
     Thread Control Block (TCB) used in thread scheduling simulations
     '''
     def __init__(self, thread_id: str, arrival_time: int, burst_time: int, priotity: int):
-        # Static attrivutes from input
-        self.thread_id = thread_id
-        self.arrival = arrival_time     # when the thread arrives
-        self.burst = burst_time         # how long it needs the CPU
-        self.priority = priotity        # for priority scheduling
+        # Static attributes from input
+        self.thread_id: str = thread_id
+        self.arrival: int = arrival_time     # when the thread arrives
+        self.burst: int = burst_time         # how long it needs the CPU
+        self.priority: int = priotity        # for priority scheduling
 
         # Simulation state
-        self.remaining = burst_time     # decrementing as thread runs
-        self.start_time = None          # first time the thread gets CPU
-        self.completion_time = None     # time when thread finishes execution
+        self.remaining: int = burst_time     # decrementing as thread runs
+        self.start_time: int = 0          # first time the thread gets CPU
+        self.completion_time: int = 0     # time when thread finishes execution
 
         # Metrics
-        self.waiting_time = 0
-        self.turnaround_time = 0
+        self.waiting_time: int = 0
+        self.turnaround_time: int = 0
 
         # internal booking-keeping fields
-        self.last_run_time = None # useful on RR and SJF
+        self.last_run_time: int = 0 # useful on RR and SJF
 
-    def is_finished (self): 
-        ''' 
+    def is_finished(self):
+        '''
             Rerturns True if the thread has finished execution (no more burst time remaining)
         '''
-        return self.remaining <= 0 
+        return self.remaining <= 0
 
-    def Compute_metrics(self): 
+    def compute_metrics(self):
         '''
             Computes turnaround time and waiting time for the thread after completion
         '''
