acc1c02570276990170c29a8d14d714615d26358
Alejandromarchesini12 <alejandro.marchesini12@gmail.com>
2025-11-18 22:21:24 -0800

    Add multilevel queue to __init__

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,5 +3,6 @@ from .first_come_first_serve import FCFS
 from .round_robin import RR
 from .shortest_job_first import SJF
 from .priority import Priority
+from .multilevel_queue import MultilevelQueue
 
-__all__ = ['Algorithm', 'FCFS', 'RR', 'SJF', 'Priority']
+__all__ = ['Algorithm', 'FCFS', 'RR', 'SJF', 'Priority', 'MultilevelQueue']
